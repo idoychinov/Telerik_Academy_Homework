51ccef50b4d32fd1a6de38963485923d439e583f
idoychinov <doychinov.ivan@gmail.com>
2013-09-10 00:32:38 +0300

    update

--- C Sharp 2/Exam Preparation/CSharp2_05022013/P04CSharpBrackets/P04CSharpBrackets.cs
+++ C Sharp 2/Exam Preparation/CSharp2_05022013/P04CSharpBrackets/P04CSharpBrackets.cs
@@ -4,42 +4,101 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace P04CSharpBrackets
 {
+    /// <summary>
+    /// INCOMPLEATE  60/100
+    /// </summary>
     class P04CSharpBrackets
     {
         static void Main()
         {
-            int N = int.Parse(Console.ReadLine());
-            string indentation = Console.ReadLine();
-            string[] input=new string[N];
-            for (int i = 0; i < N; i++)
+            if (File.Exists("tin.txt"))
             {
-                string temp = Console.ReadLine();
-                input[i]= Regex.Replace(temp,@"\s+"," "); 
+                Console.SetIn(new StreamReader("tin.txt"));
             }
 
-            int currentIndent = 0;
-            Queue<string> output= new Queue<string>();
+            int N = int.Parse(Console.ReadLine());
+            string indentation = Console.ReadLine();
+            string[] input = new string[N];
+            List<string> clearInput = new List<string>();
             for (int i = 0; i < N; i++)
             {
-                StringBuilder indent = new StringBuilder();
-                for (int j = 0; j < currentIndent; j++)
+                string line = Console.ReadLine();
+                string temp=Regex.Replace(line, @"\s+", " ");
+                if(temp!=string.Empty)
                 {
-                    indent.Append(indentation);
+                    clearInput.Add(temp);
                 }
-                output.Enqueue(indent.ToString());
-                int searchindex = input[i].IndexOf('{');
-                while(true)
+            }
+
+            input = clearInput.ToArray();
+            N = input.Length;
+            
+
+            int currentIndent = 0;
+            Queue<string> output = new Queue<string>();
+            for (int i = 0; i < N; i++)
+            {
+                int index = 0;
+                string currentLine = input[i];
+                StringBuilder tempText = new StringBuilder();
+                while (index < input[i].Length)
                 {
+                    // somewhere here empty indented row is added before brackets 
+                    char currentChar = currentLine[index];
+
+                    if (currentChar == '{' || currentChar == '}')
+                    {
+                        StringBuilder indentAddition = new StringBuilder(currentIndent * indentation.Length);
+                        for (int j = 0; j < currentIndent; j++)
+                        {
+                            indentAddition.Append(indentation);
+                        }
+                        if (tempText.Length != 0)
+                        {
+                            output.Enqueue(indentAddition.ToString() + tempText.ToString().Trim());
+                            output.Enqueue("\n");
+                        }
+                        if (currentChar == '}')
+                        {
+                            currentIndent--;
+                            indentAddition.Remove(indentAddition.Length - indentation.Length, indentation.Length);
+                        }
+                        output.Enqueue(indentAddition.ToString() + currentChar.ToString());
+                        output.Enqueue("\n");
+                        if (currentChar == '{')
+                        {
+                            currentIndent++;
+                        }
+                        index++;
+                        tempText.Clear();
+                    }
+                    else
+                    {
+                        tempText.Append(currentChar);
+                        index++;
+                    }
                 }
+                if(tempText.Length !=0)
+                {
+                    StringBuilder indentAddition = new StringBuilder(currentIndent * indentation.Length);
+                    for (int j = 0; j < currentIndent; j++)
+                    {
+                        indentAddition.Append(indentation);
+                    }
+                    output.Enqueue(indentAddition.ToString() + tempText.ToString().Trim());
+                    output.Enqueue("\n");
+                }
+
             }
 
 
             PrintResult(output);
         }
-  
+
         private static void PrintResult(Queue<string> output)
         {
             StringBuilder ouputline = new StringBuilder();
