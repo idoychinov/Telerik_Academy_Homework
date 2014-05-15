c326928d3e574f068166d26bb20eda4f6ffe1022
idoychinov <doychinov.ivan@gmail.com>
2014-05-15 20:30:05 +0300

    js exam prep

---
+++ JS Fundamentals/10.ExamPreparation/Variant1/MySolutions/Closure.js
@@ -0,0 +1,67 @@
+﻿var firstTest = [
+    '(+ 1 2)', 
+'(+ 5 2 7 1)',
+'(- 4 2)',
+'(- 4)',
+'(/ 2)'
+
+
+];
+
+
+
+function Solve(input) {
+    var operator;
+    var parameters=[];
+    for(var i=0; i<input.length;i++){
+        var currentRow = input[i].trim();
+        for (var j = 0; j < currentRow.length; j++) {
+            var currentSymbol = currentRow[j];
+            if (currentSymbol == ' ') {
+                continue;
+            }
+
+            if(isMathOperator(currentSymbol))
+            {
+                operator=currentSymbol;
+                continue;
+            }
+
+            if (!isNaN(currentSymbol)) {
+                // if(symbol == Number(symbol) )
+                parameters.push(parseInt(currentSymbol));
+            }
+
+            function calculate(operator,parameters){
+                if (parameters.length == 1) {
+                    return parameters[0];
+                }
+
+                var result = parameters[0];
+                for (var i = 0; i < length; i++) {
+                    switch (operator) {
+                        case '+': result += parameters[i]; break;
+                        case '-': result -= parameters[i]; break;
+                        case '*': result *= parameters[i]; break;
+                        case '/': if (parameters[i] == 0) {
+                            return 'Error';
+                        }
+                            result /= parameters[i];
+                            result = parseInt(result);
+                            break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    function isMathOperator(symbol) {
+        if(symbol=='+' || symbol == '-'|| symbol =='*' || symbol == '/'){
+            return true;
+        }
+        return false;
+    }
+}
+
+console.log(Solve(firstTest));
---
+++ JS Fundamentals/10.ExamPreparation/Variant1/MySolutions/JoroTheRabbit.js
@@ -0,0 +1,72 @@
+﻿function Solve(input) {
+    var initialState = input[0].split(' ');
+    var N = parseInt(initialState[0]);
+    var M = parseInt(initialState[1]);
+    var J = parseInt(initialState[2]);
+
+    function Position(input) {
+        var splitInput = input.split(' ');
+        this.row = parseInt(splitInput[0]);
+        this.col = parseInt(splitInput[1]);
+    }
+
+    var startingPosition = new Position(input[1]);
+
+    var jumpSequence = [];
+
+    for (var i = 2; i < input.length; i += 1) {
+        jumpSequence.push(new Position(input[i]));
+    }
+
+    var field = [];
+    var currentNumber = 0;
+    for (var row = 0; row < N; row += 1) {
+        field[row] = [];
+        for (var col = 0; col < M; col += 1) {
+            currentNumber += 1;
+            field[row].push(currentNumber);
+        }
+    }
+
+
+    var currentPosition = {
+        row: startingPosition.row,
+        col: startingPosition.col
+    }
+    var jumpSum = field[currentPosition.row][currentPosition.col];
+    field[currentPosition.row][currentPosition.col] = 0;
+    var jumping = true;
+    var numberOfJumps = 0;
+    var sequenseCurrentJump = 0;
+
+    while (jumping) {
+
+        numberOfJumps += 1;
+        currentPosition.row += jumpSequence[sequenseCurrentJump].row;
+        currentPosition.col += jumpSequence[sequenseCurrentJump].col;
+
+        if (currentPosition.row >= N || currentPosition.col >= M || currentPosition.row <0 || currentPosition.col<0) {
+            return 'escaped ' + jumpSum;
+        }
+        
+        if (field[currentPosition.row][currentPosition.col]) {
+            jumpSum += field[currentPosition.row][currentPosition.col];
+            field[currentPosition.row][currentPosition.col] = 0;
+        } else {
+            return 'caught ' + numberOfJumps;
+        }
+
+        sequenseCurrentJump = (sequenseCurrentJump + 1) % J;
+    }
+}
+
+//var input = [
+//    '6 7 3',
+//    '0 0',
+//    '2 2',
+//    '-2 2',
+//    '3 -1'
+//]
+
+
+//console.log(Solve(input));
