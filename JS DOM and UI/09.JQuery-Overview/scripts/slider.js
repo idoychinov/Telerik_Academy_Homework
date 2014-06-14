00c767e693873e035437ac516d23097961a24abb
idoychinov <doychinov.ivan@gmail.com>
2014-06-14 19:47:19 +0300

    handlebars

---
+++ JS DOM and UI/09.JQuery-Overview/scripts/slider.js
@@ -0,0 +1,15 @@
+﻿(function($){
+    var Slider = function () {
+        var currentIndex = 0;
+
+        function init(selector, interval, elements) {
+            var $slider = $(selector);
+
+        }
+
+
+        return {
+            init : init
+        }
+    }
+}(JQuery))
---
+++ JS DOM and UI/11.HTML-Templates Handlebars/scripts/listview-plugin.js
@@ -0,0 +1,44 @@
+﻿/// <reference path="jquery-2.1.1.min.js" />
+window.onload = function () {
+    (function ($) {
+        var books = [
+            { id: 1, title: "JavaScript: The Good parts" },
+            { id: 2, title: "Secrets of the Java Script Ninja" },
+            { id: 3, title: "Core HTML5 Canvas" },
+            { id: 4, title: "JavaScript Patterns" },
+        ],
+            students = [
+                { number: 1, name: "Peter Petrov", mark: 5.5 },
+                { number: 2, name: "Stamat Georgiev", mark: 4.7 },
+                { number: 3, name: "Maria Todorova", mark: 6 },
+                { number: 4, name: "Georgi Geshov", mark: 3.7 },
+            ]
+
+        $.fn.listview = function (collection) {
+            var $this = $(this),
+                $dataTemplate,
+                template;
+
+            if ($this.attr("data-template")) {
+                $dataTemplate = $("[id=" + $this.attr("data-template") + "]");
+            } else {
+                //Task 4
+                $dataTemplate = $this;
+                console.log($this.html())
+            }
+
+            $dataTemplate.prepend("{{#each $reserved$For$Collection$}}");
+            $dataTemplate.append("{{/each}}");
+
+            template = Handlebars.compile($dataTemplate.html());
+            $this.empty();
+            $this.append(template({$reserved$For$Collection$ :collection}));
+            return $this;
+        }
+
+        $("#books-list").listview(books);
+        $("#students-table").listview(students);
+        $('#students-table-inline').listview(students);
+
+    }(jQuery));
+}
