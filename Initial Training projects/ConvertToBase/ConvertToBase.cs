a5c1a21f15a2ba1d769b09731be653e07a4a7907
idoychinov <doychinov.ivan@gmail.com>
2014-06-25 08:51:52 +0300

    moved innitial training projects

---
+++ Initial Training projects/ConvertToBase/ConvertToBase.cs
@@ -0,0 +1,99 @@
+﻿/* Problem 7. Write a program to convert from any numeral system of given base s to any other numeral system of base d (2 ≤ s, d ≤  16).
+ */
+
+using System;
+
+class ConvertToAnyBase
+{
+    static char[] mapping = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+    static void Main()
+    {
+
+        string input;
+        int s, d;
+
+        Console.Write("Enter the input number base (between 2 and 16):");
+        s = ValidateBases();
+
+        Console.Write("Enter the input number (use symbols from  0 to {0}:", mapping[s - 1]);
+        input = ValidateInput(s);
+
+        Console.Write("Enter the output number base (between 2 and 16):");
+        d = ValidateBases();
+
+        Console.WriteLine("The number {0} in {1} base is represented in {2} based system as {3}", input, s, d, ConvertFromDecimalBased(ConvertToDecimalBased(input, s), d));
+    }
+
+    private static int ValidateBases()
+    {
+        int output;
+        bool valid = false;
+        while (true)
+        {
+            valid = int.TryParse(Console.ReadLine(), out output) && (output >= 2) && (output <= 16); //input validation
+            if (valid)
+            {
+                break;
+            }
+            Console.WriteLine("Incorect input, please try again.");
+        }
+        return output;
+    }
+
+    private static string ValidateInput(int lenght)
+    {
+        string output;
+        char[] localMapping = new char[lenght];
+        Array.Copy(mapping, localMapping, lenght);
+        while (true)
+        {
+            bool valid = true;
+            output = Console.ReadLine();
+            foreach (char item in output)  // check if all symbols in the input are part of the predefined mapping of allowed symbols
+            {
+                if (Array.BinarySearch(localMapping, item) < 0)
+                {
+                    valid = false;
+                }
+            }
+            if (valid)
+            {
+                break;
+            }
+            Console.WriteLine("Incorect input, please try again.");
+        }
+        return output;
+    }
+
+    private static int ConvertToDecimalBased(string input, int inbase)
+    {
+        int output = 0;
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            output += Array.BinarySearch(mapping, input[i]) * (int)Math.Pow(inbase, input.Length - 1 - i);
+        }
+
+        return output;
+    }
+
+    private static string ConvertFromDecimalBased(int number, int inbase)
+    {
+        string output = "";
+        while (true)
+        {
+            output = mapping[number % inbase] + output;
+            number /= inbase;
+            if (number == 0)
+            {
+                break;
+            }
+        }
+
+        return output;
+    }
+
+
+
+}
+
---
+++ Initial Training projects/NumberToText/NumberToText.cs
@@ -0,0 +1,129 @@
+﻿/* Problem 11. Write a program that converts a number in the range [0...999] to a text corresponding to its English pronunciation. Examples:
+ * 0  "Zero"
+ * 273  "Two hundred seventy three"
+ * 400  "Four hundred"
+ * 501  "Five hundred and one"
+ * 711  "Seven hundred and eleven"
+
+ */
+using System;
+using MyIO;
+
+class NumberToText
+{
+    static string UpToNineghteen(int num)
+    {
+        switch (num)
+        {
+            case 1: return "One";
+            case 2: return "Two";
+            case 3: return "Three";
+            case 4: return "Four";
+            case 5: return "Five";
+            case 6: return "Six";
+            case 7: return "Seven";
+            case 8: return "Eight";
+            case 9: return "Nine";
+            case 11: return "Eleven";
+            case 12: return "Twelve";
+            case 13: return "Thirteen";
+            case 14: return "Fourteen";
+            case 15: return "Fifteen";
+            case 16: return "sixteen";
+            case 17: return "seventeen";
+            case 18: return "eighteen";
+            case 19: return "nineteen";
+            default: return "";
+        }
+    }
+    static string Tens(int tens)
+    {
+        switch (tens)
+        {
+            case 10: return "Ten";
+            case 20: return "Twenty";
+            case 30: return "Thirty";
+            case 40: return "Fourty";
+            case 50: return "Fifty";
+            case 60: return "Sixty";
+            case 70: return "Seventy";
+            case 80: return "Eighty";
+            case 90: return "Ninety";
+            default: return "";
+        }
+    }
+    static void Main()
+    {
+        ushort number;
+        string textNumber = "";
+        bool hundreds = false; ;
+        MyIOClass ConsoleIO = new MyIOClass();
+
+        while (true)
+        {
+            ConsoleIO.Input(out number, "Please enter a number in the range [0...999]: ");
+            if (number < 1000)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("\nThe value you entered is incorect!\n");
+            }
+        }
+
+        if (number == 0)
+        {
+            Console.WriteLine("The number is: Zero");
+        }
+        else
+        {
+            if (number > 99)
+            {
+                hundreds = true;
+                textNumber = UpToNineghteen(number / 100) + " hundred";
+                number %= 100;
+            }
+            if (number % 10 == 0)
+            {
+                if (hundreds)
+                {
+                    if (number != 0)
+                    {
+                        textNumber = textNumber + " and " + Tens(number);
+                    }
+                }
+                else
+                {
+                    textNumber = Tens(number);
+                }
+            }
+            else if (number < 20)
+            {
+                if (hundreds)
+                {
+                    textNumber = textNumber + " and " + UpToNineghteen(number);
+                }
+                else
+                {
+                    textNumber = UpToNineghteen(number);
+                }
+            }
+            else
+            {
+                if (hundreds)
+                {
+                    textNumber = textNumber + " and " + Tens((number / 10) * 10) + " " + UpToNineghteen(number % 10);
+                }
+                else
+                {
+                    textNumber = Tens((number / 10) * 10) + " " + UpToNineghteen(number % 10);
+                }
+            }
+            Console.WriteLine("The number is: {0}", textNumber);
+        }
+
+
+    }
+}
+
